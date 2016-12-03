0ca5e709b95a088a5cdd71d5ab42f321e3b24c8f
Daniel Modrego <modregod@gmail.com>
2016-12-03 14:12:26 +0100

    Array de meses en castellano php

---
+++ array_meses.php
@@ -0,0 +1,4 @@
+<?php
+$dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
+$meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+?>
