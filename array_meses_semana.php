b0ff27121b8cfa2614f1378761206108931a72c8
Daniel Modrego <modregod@gmail.com>
2017-08-22 14:58:15 +0200

    Update array_meses_semana.php

--- array_meses_semana.php
+++ array_meses_semana.php
@@ -1,4 +1,4 @@
 <?php
 $dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
-$meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+$meses = array(1 => "Enero",2 => "Febrero",3 => "Marzo",4 => "Abril",5 => "Mayo",6 => "Junio",7 => "Julio",8 => "Agosto",9 => "Septiembre",10 => "Octubre",11 => "Noviembre",12 => "Diciembre");
 ?>
